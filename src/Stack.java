f8a430eceaa4bfcd6796da968d70cf5ea6ffe477
robwrobel <robwrobel@gmail.com>
2018-01-25 16:13:20 +0100

    Push (#2)
    * push added
    
    * Java docs added

--- src/Stack.java
+++ src/Stack.java
@@ -1,9 +1,22 @@
 import java.lang.reflect.Array;
 
+/**
+ * 
+ * @param <T>
+ */
 public class Stack<T> {
     private T[] table;
-    
+    int index;
+    int size;
     public Stack (Class<T> c, int size) {
-        table = (T[]) Array.newInstance(c,size);
+        this.table = (T[]) Array.newInstance(c,size);
+        this.size = size;
+        this.index = 0;
+    }
+
+    void push (T element) {
+        if (index >= size) throw new StackOverflowException();
+        table[index] = element;
+        index++;
     }
 }

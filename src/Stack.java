a8da3c9beb666c4710f6fa0dce328d17fdbdbfe5
Robert Wrobel <robwrobel@gmail.com>
2018-01-25 17:41:10 +0100

    pop added

--- src/Stack.java
+++ src/Stack.java
@@ -2,8 +2,16 @@ import java.lang.reflect.Array;
 
 public class Stack<T> {
     private T[] table;
-    
+    int index;
+    int size;
     public Stack (Class<T> c, int size) {
         table = (T[]) Array.newInstance(c,size);
+        this.size = size;
+        this.index = 0;
+    }
+
+    T pop() {
+        if (index == 0) throw new StackUnderflowException();
+        return table[--index];
     }
 }
